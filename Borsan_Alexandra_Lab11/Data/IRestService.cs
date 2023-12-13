b50b7268c1bef0fc2f04a5f87c2dee2ebb8f31a0
Alexandra Borsan <alexandra.borsan@gmail.com>
2023-12-13 17:28:28 +0200

    Add project files.

---
+++ Borsan_Alexandra_Lab11/Data/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Borsan_Alexandra_Lab11;
+using Borsan_Alexandra_Lab11.Models;
+
+namespace Borsan_Alexandra_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
